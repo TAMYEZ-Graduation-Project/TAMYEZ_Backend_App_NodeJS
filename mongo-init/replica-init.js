97addd9abd4da9b4112fae4d05ae55a1606e7efb
Mohammed Khalil <klilmohammed9@gmail.com>
2026-02-20 22:13:55 +0200

    refactor: remove unneeded folder

--- mongo-init/replica-init.js
+++
@@ -1,8 +0,0 @@
-print("⚙️  Initializing Replica Set (initdb.d script) ...");
-
-rs.initiate({
-  _id: "rs0",
-  members: [{ _id: 0, host: "mongo:27017" }]
-});
-
-print("✅ Replica Set Initialized");
